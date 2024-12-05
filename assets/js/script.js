6324fd8f8b39b70406c28f7a525cbf9ef82b5fbb
Tinashe Mapurisa <1.1.1.1.34tinashe@gmail.com>
2024-12-05 06:24:08 +0200

    Made some very important changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,17 @@ function vibrate() {
   navigator.vibrate(6);
 }
 
-// Zoom in up effect add in every social media button one by one
-const socialMediaLinks = document.querySelectorAll(".social-media div");
+// // Zoom in up effect add in every social media button one by one
+// const socialMediaLinks = document.querySelectorAll(".social-media div");
 
-// ON SCROLL REVEAL SMALL CARDS
-ScrollReveal().reveal(socialMediaLinks, {
-  duration: 1000,
-  opacity: 0,
-  distance: "30%",
-  origin: "bottom",
-  scale: 0.9,
-});
+// // ON SCROLL REVEAL SMALL CARDS
+// ScrollReveal().reveal(socialMediaLinks, {
+//   duration: 1000,
+//   opacity: 0,
+//   distance: "30%",
+//   origin: "bottom",
+//   scale: 0.9,
+// });
 
 // on load name effect 
 setTimeout(() => document.querySelector('.name').classList.add('loaded'), 500);
